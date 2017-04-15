0661095100503abfdb92861f51ef5aac076b9b54
garbageman <dnagle@umd.edu>
2017-04-15 14:51:08 -0400

    Started adding clientside javascript files and login page

---
+++ xampp/js/LoginServices.js
@@ -0,0 +1,4 @@
+/* This is a service that allows for validation that a login */
+/* is correct */
+
+var loginService = {};
---
+++ xampp/js/config.js
@@ -0,0 +1,7 @@
+/* This config file is basically just a huge JSON */
+/* it will store most of the information that we use */
+/* for db connections etc. */
+
+var CONFIG = {};
+
+CONFIG.baseTestURL = "localhost/groupProject";
