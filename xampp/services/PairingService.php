44827cd0b557a8e2a378908179772a4b97393fc3
merlinlevine <merlinlevine@gmail.com>
2017-04-30 20:59:10 -0400

    Updated PairingService.php with query information.

--- xampp/services/PairingService.php
+++ xampp/services/PairingService.php
@@ -1,8 +1,24 @@
 <?php
-  header('Content-Type: application/json');
-  require('connection.php');
-  $db = connectToTable("testUsers");
+	header('Content-Type: application/json');
+	require('connection.php');
+	$db = establishConnection();
 
+	// wine and cheese parameters will always be set
+	$wine = $_GET["wine"];
+   $cheese = $_GET["cheese"];
 
-  
+	// table names
+	$wineTable = "wine";
+	$cheeseTable = "cheese";
+	$pairingTable = "pairing";
+
+	// $genericQuery = "select * from %s where %s = '%s'";
+	$wineQuery = "select * from $wineTable where wine = '$wine'";
+	$cheeseQuery = "select * from $cheeseTable where cheese = '$cheese'";
+	$pairingQuery = "select * from $pairingTable where wine = '$wine' AND cheese = '$cheese'";
+
+	$wineResult = $db->query($wineQuery);
+	$cheeseResult = $db->query($cheeseQuery);
+	$pairingResult = $db->query($pairingQuery);
+
 ?>
