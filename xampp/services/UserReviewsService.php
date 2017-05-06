739b208a707d6c484b609025ec7f4b887910f12c
garbageman <dnagle@umd.edu>
2017-05-06 16:08:31 -0400

    updated config file and added url parameter function

---
+++ xampp/services/UserReviewsService.php
@@ -0,0 +1,35 @@
+<?php
+  header('Content-Type: application/json');
+  require('connection.php');
+  $db = establishConnection();
+  // First check that the user supplied doesn't already exist
+  $userName = $_GET["username"];
+  $table = "reviews";
+
+  $sqlQuery = sprintf("select * from %s where username=\"%s\"", $table,$userName);
+	$result = mysqli_query($db, $sqlQuery);
+  if ($result) {
+    $numberOfRows = mysqli_num_rows($result);
+    if ($numberOfRows > 0) {
+        /* Create a list of reviews */
+        
+        echo json_encode("{ \"result\" : false, \"message\" : \"Username already exists\"}");
+    } else {
+      /* There are no users with this name so create a new item */
+      $encryptedPassword = password_hash($password, PASSWORD_BCRYPT);
+      $sqlQuery = sprintf("insert into %s values (\"%s\",\"%s\")", $table,$userName,$encryptedPassword);
+      $result = mysqli_query($db, $sqlQuery);
+      if ($result) {
+        /* Return a token and username validating the session */
+        $response = sprintf("{ \"result\" : true, \"username\" : \"%s\" }", $userName);
+        echo json_encode($response);
+      } else {
+        $response = sprintf("{ \"result\" : false, \"message\" : \"%s\"}",mysqli_error($db));
+        echo json_encode($response);
+      }
+    }
+  } else {
+    // Something went horribly wrong with the search so log the error
+    echo json_encode("{ result : false, message : \"There was an issue with the query\"}");
+  }
+ ?>
---
+++ xampp/services/cheeseImage.php
@@ -0,0 +1,31 @@
+<?php
+  header("Content-type: image/jpeg");
+  require('connection.php');
+  $db = establishConnection();
+  // First check that the user supplied doesn't already exist
+  $cheese = $_GET["cheese"];
+  $table = "cheeseDummy";
+
+  $sqlQuery = sprintf("select * from %s", $table,$cheese);
+	$result = $db->query($sqlQuery);
+  if ($result) {
+    //echo "got a result";
+    $numberOfRows = mysqli_num_rows($result);
+    if ($numberOfRows > 0) {
+        /* Create a list of reviews */
+        while ($recordArray = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+          echo $recordArray['image'];
+        }
+        // $result->data_seek(0);
+        // $cheeseRow = $result->fetch_array(MSQLI_ASSOC);
+        // echo $cheeseRow["name"];
+        // echo $cheeseRow["image"];
+    } else {
+      /* There are no results */
+
+    }
+  } else {
+    // Something went horribly wrong with the search so log the error
+    echo json_encode("{ result : false, message : \"There was an issue with the query\"}");
+  }
+ ?>
