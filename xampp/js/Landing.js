1375398d86c76c7c46e0d6fd50b679d963f2547b
garbageman <dnagle@umd.edu>
2017-05-08 10:46:26 -0400

    Allowed search on pressing enter

--- xampp/js/Landing.js
+++ xampp/js/Landing.js
@@ -12,6 +12,21 @@ $("#searchButton").click(function() {
   window.location.href = str;
 });
 
+var handle = function(e) {
+  if(e.keyCode === 13){
+    e.preventDefault();
+    let items = query.split(" & ");
+    let str = "searchResult.html?item1=";
+    if (items.length > 0) {
+      str = str + items[0].replace(" ","+").trim();
+      if (items.length > 1) {
+        str =  str + "&item2="+ items[1].replace(" ","+").trim();
+      }
+    }
+    window.location.href = str;
+  }
+};
+
 // // Function that creates the innerHTML of
 // let createThumbnail = function(wine, cheese) {
 //
